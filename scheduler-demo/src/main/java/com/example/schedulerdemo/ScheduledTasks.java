2eeda48d68db20258a32e34246e7e68e245f592b
gabikersul <54965207+gabikersul@users.noreply.github.com>
2021-04-15 13:15:20 -0300

    Add files via upload

---
+++ scheduler-demo/src/main/java/com/example/schedulerdemo/ScheduledTasks.java
@@ -0,0 +1,55 @@
+package com.example.schedulerdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+//    In the following example, The annotated method will be executed every 2 seconds.
+
+    @Scheduled(fixedRate = 2000)
+    public void scheduleTaskWithFixedRate() {
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+        logger.info("Current Thread : {}", Thread.currentThread().getName());
+    }
+
+//    The fixedDelay parameter counts the delay after the completion of the last invocation.
+
+    @Scheduled(fixedDelay = 2000)
+    public void scheduleTaskWithFixedDelay() {
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        logger.info("Current Thread : {}", Thread.currentThread().getName());
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException ex) {
+            logger.error("Ran into an error {}", ex);
+            throw new IllegalStateException(ex);
+        }
+    }
+
+//    In the following example, the first execution of the task will be delayed by 5 seconds and
+//    then it will be executed normally at a fixed interval of 2 seconds
+
+    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+    public void scheduleTaskWithInitialDelay() {
+        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        logger.info("Current Thread : {}", Thread.currentThread().getName());
+    }
+
+//    scheduled the task to be executed every minute
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void scheduleTaskWithCronExpression() {
+        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
+        logger.info("Current Thread : {}", Thread.currentThread().getName());
+    }
+}
